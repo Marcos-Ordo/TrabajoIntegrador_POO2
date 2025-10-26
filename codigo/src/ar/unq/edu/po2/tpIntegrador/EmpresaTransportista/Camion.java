b09f27a4379cba7190c2503b057e2daf4a176a0d
ThomasSandovalUNQ <thomassandovala10@gmail.com>
2025-10-26 00:13:48 -0300

    Empresas Transportistas

---
+++ codigo/src/ar/unq/edu/po2/tpIntegrador/EmpresaTransportista/Camion.java
@@ -0,0 +1,15 @@
+package ar.unq.edu.po2.tpIntegrador.EmpresaTransportista;
+
+public class Camion {
+	
+	private String modelo;
+
+	public Camion(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+	
+}
---
+++ codigo/src/ar/unq/edu/po2/tpIntegrador/EmpresaTransportista/Chofer.java
@@ -0,0 +1,14 @@
+package ar.unq.edu.po2.tpIntegrador.EmpresaTransportista;
+
+public class Chofer {
+	private String nombre;
+
+	public Chofer(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+		
+}
